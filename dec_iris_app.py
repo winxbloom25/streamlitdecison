45889fed56c9d905728500370f5a5b186a4aa957
Kinjal Patil <147801891+winxbloom25@users.noreply.github.com>
2023-11-29 16:58:21 +0530

    Add files via upload

---
+++ dec_iris_app.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import pandas as pd
+import pickle
+from sklearn import datasets
+
+with open('dec_iris.pkl', 'rb') as file:
+    clf = pickle.load(file)
+
+st.write("""
+# Simple Iris Flower Prediction App
+
+This app predicts the **Iris Flower** type!
+""")
+
+st.sidebar.header('User Input Parameters')
+
+def user_input_features():
+    sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.4)
+    sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.4)
+    petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 1.3)
+    petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 0.2)
+    data = {'sepal_length': sepal_length,
+            'sepal_width': sepal_width,
+            'petal_length': petal_length,
+            'petal_width': petal_width}
+    features = pd.DataFrame(data, index=[0])
+    return features
+
+df = user_input_features()
+
+st.subheader('User Input parameters')
+st.write(df)
+
+iris = datasets.load_iris()
+
+prediction = clf.predict(df)
+prediction_proba = clf.predict_proba(df)
+
+st.subheader('Prediction and Prediction Probability')
+proba_df = pd.DataFrame(prediction_proba, columns=iris.target_names)
+st.write(proba_df)
+
+st.write(f"Class predicted is: {iris.target_names[prediction]}")
